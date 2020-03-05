5f97ace49990d88f90c8de1a12d7053c10171b70
glauciocoutinho <glauciocoutinho@hotmail.com>
2020-03-05 14:18:56 -0400

    Atualization 3

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import routes from './routes';
 
 import './database';
@@ -6,18 +7,20 @@ import './database';
 class App {
   constructor() {
     this.server = express();
-
     this.middlewares();
     this.routes();
   }
 
   middlewares() {
     this.server.use(express.json());
+    this.server.use(
+      '/files',
+      express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
+    );
   }
 
   routes() {
     this.server.use(routes);
   }
 }
-
 export default new App().server;
